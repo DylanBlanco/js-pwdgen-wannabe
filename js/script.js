03d44d5e3b5eaa495081b626107e2545e169eef1
DylanBlanco <dylanblanco1198@gmail.com>
2024-07-10 14:00:40 +0200

    chiedi cognome e salva valore

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 //chiedi nome e salva valore
 const name =  prompt(`Inserisci Nome?`);
 console.log (`name:`, name);
+
+//chiedi cognome e salva valore
+const surname =  prompt(`Inserisci Cognome?`);
+console.log (`surname:`, surname);
